f3a7d31b6b684bac62dd8ac5ea08168f5d5a21d7
arkadiusz <szastarek@gmail.com>
2019-05-13 23:33:00 +0200

    Init commit

---
+++ src/main/java/com/movie/database/movie_database/user/User.java
@@ -0,0 +1,40 @@
+package com.movie.database.movie_database.user;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+    private String email;
+    private String password;
+
+    public User(UUID id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
